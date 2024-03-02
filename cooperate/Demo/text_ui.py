14dff35380e0efbbadec16dd16b2c414f0edcde3
fkhan000 <fakharyarkhan843@gmail.com>
2024-03-02 12:00:02 -0500

    created intro text to demo

---
+++ cooperate/Demo/text_ui.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+
+
+if __name__ == "__main__":
+
+    with open("logo.txt", "r") as f:
+        print(f.read())
+
+    print("\n*****Fakharyar Khan*****")
+    print("*****Nicholas Singh*****")
+    print("*****Irene Choi*********")
+    print("*****Hengnan Ma*********")
+    
+    print("\n\n**********Welcome to Cooperate**********\n\n")
+    print("This is a service that allows Cooper Union students to anonymously")
+    print("post reviews on courses and professors that they have had as well")
+    print("as upload helpful documents such as syllabi and past exams.\n")
+        
